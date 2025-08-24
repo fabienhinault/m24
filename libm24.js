5d626ec1ccf2cc9f2bde77cb8af0935bbc03ab87
Fabien Hinault <fabien.hinault@free.fr>
2025-08-24 20:44:18 +0200

    model

---
+++ libm24.js
@@ -0,0 +1,94 @@
+function range(size, startAt = 0) {
+    return [...Array(size).keys()].map(i => i + startAt);
+}
+
+function permute(array, permutation) {
+    return permutation.map(permutationIndex => array[permutationIndex]);
+}
+
+function equalArrays(array1, array2) {
+    return array1.length === array2.length &&
+        array1.every((v, i) => v === array2[i]);
+}
+
+
+function arrayStartsWith(tested, starter) {
+    return equalArrays(tested.slice(0, starter.length), starter);
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function pick(array) {
+    return array[getRandomInt(0, array.length)];
+}
+function greatestMultipleLessThan(k, sup) {
+    return sup - (sup % k);
+}
+
+function getRemainingDurationUnits(allSmalls, smallsInBig) {
+    const remainingSmalls = allSmalls % smallsInBig;
+    return {remainingSmalls, "allBigs": (allSmalls - remainingSmalls) / smallsInBig};
+}
+
+function threeDigits(n) {
+    return n.toLocaleString('fr', {minimumIntegerDigits: 3, useGrouping:false});
+}
+
+function twoDigits(n) {
+    return n.toLocaleString('fr', {minimumIntegerDigits: 2, useGrouping:false});
+}
+
+function formatDuration(allMilliseconds, decimalSeparator) {
+    const {"remainingSmalls": remainingMillis, "allBigs": allSeconds} = getRemainingDurationUnits(allMilliseconds, 1000);
+    const {"remainingSmalls": remainingSeconds, "allBigs": allMinutes} = getRemainingDurationUnits(allSeconds, 60);
+    const {"remainingSmalls": remainingMinutes, "allBigs": allHours} = getRemainingDurationUnits(allMinutes, 60);
+    let result = `${twoDigits(remainingSeconds)}${decimalSeparator}${threeDigits(remainingMillis)}`
+    if (allMinutes !== 0) {
+        result = `${twoDigits(remainingMinutes)}:` + result;
+    }
+    if (allHours !== 0) {
+        result = `${twoDigits(allHours)}:` + result;
+    }
+    return result;
+}
+
+function getPermutationInverseRaw(rawPermutedArray) {
+    let result = [];
+    rawPermutedArray.forEach((element, index) => {result[element] = index;});
+    return result;
+}
+
+function getRandomLrsString(len) {
+    if (!len) {
+        len = getRandomInt(10, 100);
+    }
+    let result = "";
+    for(let iTime = 0; iTime < len; iTime++){
+        result += pick(["L", "R", "S"]);
+    }
+    return result;
+}
+
+function L([a0, a1, ...a2_23]) {
+    return [a0, ...a2_23, a1];
+}
+
+function R([a0, ...a1_23]) {
+    return [a0,  a1_23[22], ...a1_22.slice(0, 21)];
+}
+
+function S([a0,a1,a2,a3,a4,a5,a6,a7,a8,a9,
+    a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,
+    a20,a21,a22,a23]) {
+        return [a1,a0,a23,a4,a3,a22,a11,a8,a7,a10,
+            a9,a6,a21,a14,a13,a20,a17,a16,a19,a18,
+            a15,a12,a5,a2];
+}
+
+function equalsRawGoal(rawNumbers) {
+    return equalArrays(rawNumbers, range(23));
+}
+
+export {L, R, S};
---
+++ m24Model.js
@@ -0,0 +1,122 @@
+import { L, R, S } from "./libm24.js";
+
+
+class Chrono {
+    constructor() {
+        this.startTime = 0;
+    }
+
+    start() {
+        this.startTime = Date.now();
+    }
+
+    stop() {
+        return Date.now() - this.startTime;
+    }
+}
+
+class Model {
+    constructor(frame, evtDispatcher) {
+        this.dispatcher = evtDispatcher;
+        this.previousNumbers = [];
+        this.numbers = range(this.N, 1);
+        this.lasts = [];
+        this.dispatcher.addEventListener('numbers changed', evt => {this.updateSolution();});
+        this.chrono = new Chrono();
+    }
+
+    dispatch(type, detail) {
+        return this.dispatcher.dispatchEvent(new CustomEvent(type, {detail: detail}));
+    }
+
+    dispatchLastsChanged() {
+        return this.dispatcher.dispatchEvent(new CustomEvent("lasts changed", {detail: {lasts: this.lasts}}));
+    }
+
+    pushLasts(last) {
+        this.lasts.push(last);
+        return this.dispatchLastsChanged();
+    }
+
+    popLasts() {
+        let popped = this.lasts.pop();
+        this.dispatchLastsChanged();
+        return popped;
+    }
+
+    setLasts(newLasts) {
+        this.lasts = newLasts
+        return this.dispatchLastsChanged();
+    }
+
+    setNumbers(newNumbers) {
+        if (this.frame.equalsPrettyGoal(newNumbers)) {
+            const time = this.chrono.stop();
+            this.dispatcher.dispatchEvent(
+                new CustomEvent("solved", {detail: {time: time}}));
+        }
+        this.numbers = newNumbers;
+    }
+
+    silentL() {
+        this.pushLasts('L');
+        this.previousNumbers = [...this.numbers];
+        this.setNumbers(L(this.numbers));
+    }
+
+    L() {
+        this.silentL();
+        return this.dispatcher.dispatchEvent(
+            new CustomEvent("numbers changed", {detail: {numbers: this.numbers}}));
+    }
+
+    silentR() {
+        this.pushLasts('R');
+        this.previousNumbers = [...this.numbers];
+        this.setNumbers(R(this.numbers));
+    }
+
+    R() {
+        this.silentR();
+        return this.dispatcher.dispatchEvent(
+            new CustomEvent("numbers changed", {detail: {numbers: this.numbers}}));
+    }
+
+    silentS() {
+        this.pushLasts('S');
+        this.previousNumbers = [...this.numbers];
+        this.setNumbers(S(this.numbers));
+    }
+
+    S() {
+        this.silentR();
+        return this.dispatcher.dispatchEvent(
+            new CustomEvent("numbers changed", {detail: {numbers: this.numbers}}));
+    }
+
+    applyString(str) {
+        for (const c of str) {
+            if (c !== 'L' && c !== 'R' && c !== 'S') {
+                throw new Error("invalid argument");
+            }
+            this[c]();
+        }
+    }
+
+    shuffleDefault() {
+        this.shuffleNTimes(getRandomInt(100, 200));
+    }
+
+    shuffleNTimes(nbTimes) {
+        for(let iTime = 0; iTime < nbTimes; iTime++){
+            pick([() => this.silentL(), () => this.silentR(), () => this.silentS()])();
+        }
+    }
+
+    reset() {
+        this.setNumbers(range(23));
+        this.setLasts([]);
+    }     
+};
+
+export {Chrono, Model};
